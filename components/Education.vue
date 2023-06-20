fefb462b7b49f5519c02c702ce0335c7721c6671
Abukarimov <abdukarimovmuslimbek2004@gmail.com>
2023-06-20 17:34:14 +0500

    eduaction end

---
+++ components/Education.vue
@@ -0,0 +1,70 @@
+<template>
+    <section class="bg-[#181824] h-[1000px]">
+        <div class="container mx-auto">
+            <div>
+                <h1 class="text-white text-[50px] text-center">Education</h1>
+                <div class="grid grid-cols-3 gap-[100px] mt-[100px]">
+                    <div class="work relative rounded-[10px] overflow-hidden bg-[#3d3473] flex items-center justify-center">
+                        <img src="../public/images/tatu.png" class="w-[360px] h-[360px]" alt="">
+                        <div class="text-white layer absolute top-0 w-full h-0 rounded-[10px] items-center justify-center flex flex-col gap-[10px]">
+                            <p class="text-[18px]">September 2021 - to Now</p>
+                            <h1 class="text-center text-[28px]">Tashkent University of <br> Information Technologies</h1>
+                            <h2 class="text-[22px]">Cyber security</h2>
+                            <a class="mb-[220px] w-[60px] bg-white mt-[10px] h-[60px] rounded-full flex items-center justify-center" href="https://tuit.uz/"><i class='bx bx-link-external text-[30px] text-[#7562E0]'></i></a>
+                        </div>
+                    </div>
+                    <div class="work relative rounded-[10px] overflow-hidden">
+                        <img src="../public/images/najot-2.png" class="w-full " alt="">
+                        <div class="text-white layer absolute top-0 w-full h-0 rounded-[10px] items-center justify-center flex flex-col gap-[10px]">
+                            <p class="text-[18px]">June 2022 - July 2023</p>
+                            <h1 class="text-center text-[28px]">"Najot Ta'lim" Learning center</h1>
+                            <h2 class="text-center text-[22px]">Full Stack developer <br> (Nodejs + Vuejs)</h2>
+                            <a class="mb-[220px] w-[60px] bg-white mt-[10px] h-[60px] rounded-full flex items-center justify-center" href="https://najottalim.uz/"><i class='bx bx-link-external text-[30px] text-[#7562E0]'></i></a>
+                        </div>
+                    </div>
+                    <div class="work relative rounded-[10px] overflow-hidden">
+                        <img src="../public/images/oxford.jpg" class="w-full" alt="">
+                        <div class="text-white layer absolute top-0 w-full h-0 rounded-[10px] items-center justify-center flex flex-col gap-[10px]">
+                            <p class="text-[18px]">the near Future</p>
+                            <h1 class="text-center text-[28px]">University of Oxford</h1>
+                            <h2 class="text-center text-[22px]"> Computer Science & <br> Information Systems</h2>
+                            <a class="mb-[220px] w-[60px] bg-white mt-[10px] h-[60px] rounded-full flex items-center justify-center" href="https://najottalim.uz/"><i class='bx bx-link-external text-[30px] text-[#7562E0]'></i></a>
+                        </div>
+                    </div>
+                </div>
+                <div class="hl mt-[150px]"></div>
+            </div>  
+        </div>
+    </section>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="css" scoped>
+
+.layer{
+    background: linear-gradient(rgba(0,0,0,0.6), #3d3473);
+    transition: height 0.5s;
+}
+.work:hover img{
+    transform: scale(1.1);
+}
+.work img{
+    display: block;
+    transition: transform 0.5s;
+}
+.work:hover .layer{
+    height: 100%;
+}
+.work:hover .layer a{
+    margin-bottom: 0;
+}
+.hl {
+  border-bottom: 6px solid #7562E0;
+  width:auto;
+}
+
+
+</style>
--- components/Services.vue
+++ components/Services.vue
@@ -1,9 +1,25 @@
 <template>
-    <section class="bg-[#181824] h-[500px]">
+    <section class="bg-[#181824] h-[850px]">
         <div class="container mx-auto">
             <div class="hl"></div>
             <h1 class="text-white text-[50px] text-center mt-[100px]">My services</h1>
-            <div></div>
+            <div class="grid grid-cols-3 gap-[60px] text-white mt-[100px]">
+                <div class="border border-[5px] flex flex-col justify-center items-center pt-[70px] pb-[70px] rounded-[16px] hover:border-[#7562E0] duration-700 transition delay-150 ease-in-out hover:-translate-y-1 hover:scale-105 hover:text-[#7562E0]">
+                    <i class="bx bxl-javascript text-[80px]"></i>
+                    <h1 class="text-[28px] mt-[20px] mb-[40px]">UI & UX DESIGNING</h1>
+                    <p class="text-center text-[20px]">I design beautiful web iterfaces <br> with Figma and Adove XD</p>
+                </div>
+                <div class="border border-[5px] flex flex-col justify-center items-center pt-[70px] pb-[70px] rounded-[16px] hover:border-[#7562E0] duration-700 transition delay-150 ease-in-out hover:-translate-y-1 hover:scale-105 hover:text-[#7562E0]">
+                    <i class='bx bxl-python text-[80px]' ></i>
+                    <h1 class="text-[28px] mt-[20px] mb-[40px]">MOBILE DEVELOPMENT</h1>
+                    <p class="text-center text-[20px]">I create beautiful iterfaces with <br> simple HTML, CSS, & JavaScript <br> and also frameworks like Angular <br> and ReactJS</p>
+                </div>
+                <div class="border border-[5px] flex flex-col justify-center items-center pt-[70px] pb-[70px] rounded-[16px] hover:border-[#7562E0] duration-700 transition delay-150 ease-in-out hover:-translate-y-1 hover:scale-105 hover:text-[#7562E0]">
+                    <i class='bx bx-code-alt text-[80px]' ></i>
+                    <h1 class="text-[28px] mt-[20px] mb-[40px]">NPM AND NODEJS</h1>
+                    <p class="text-center text-[20px]">I have core understanding of NPM.<br> I can also develop general <br> purpose applications with NodeJS</p>
+                </div>
+            </div>
         </div>
     </section>
 </template>
